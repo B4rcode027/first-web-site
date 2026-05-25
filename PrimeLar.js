c7eaca1fd97d07b301b28f6e9e4e8c5935084142
Locked002 <gpereira2019@gmail.com>
2026-05-25 20:05:57 -0300

    Implement search functionality for items in PrimeLar.js

--- PrimeLar.js
+++ PrimeLar.js
@@ -1,2 +1,17 @@
-alert ("Ola Mundo!")
 
+// O JavaScript que faz a mágica acontecer
+const searchForm = document.getElementById("searchForm")
+const itens = document.querySelectorAll(".item");
+
+searchForm.addEventListener("input", (e) => {
+  const valorDigitado = e.target.value.toLowerCase()
+
+  itens.forEach((item) => {
+    // Verifica se o texto do item inclui o que foi digitado
+    if (item.textContent.toLowerCase().includes(valorDigitado)) {
+      item.style.display = "block" // Mostra o item
+    } else {
+      item.style.display = "none" // Oculta o item
+    }
+  })
+})
