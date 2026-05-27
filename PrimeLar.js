d566f618ab772da6c9956e05a3031671facb8ac8
Locked002 <gpereira2019@gmail.com>
2026-05-26 23:04:41 -0300

    Enhance search functionality by adding event listeners and updating datalist options in PrimeLar.js; modify button classes in index.html for navigation consistency.

--- PrimeLar.js
+++ PrimeLar.js
@@ -1,17 +1,51 @@
-
-// O JavaScript que faz a mágica acontecer
-const searchForm = document.getElementById("searchForm")
+// javascript para caixa de pesquisa
+let searchForm = document.getElementById("searchForm")
 const itens = document.querySelectorAll(".item");
+let botoes = document.querySelectorAll(".nav")
+console.log(botoes)
+let input = document.getElementById("searchInput")
+let datalist = document.getElementById("itens")
+console.log(datalist)
+botoes.forEach((botao, index) => {
+  let href = botao.querySelector("a").getAttribute("href")
+  datalist.innerHTML += `<option class="item" value="${botao.textContent.trim()}" href="${href}" onclick="selecionarItem('${href}')">`
+})
+// function selecionarItem(href) {
+//   console.log("selecionarItem")
+//   searchForm.setAttribute("data-search", href)
+// }
 
+searchForm.addEventListener("input",function(e) {
+  let itenss = document.querySelectorAll(".item")
+  console.log(e.target.value)
+  let valor=e.target.value.toLowerCase()
+  let selecionado = Array.from(itenss).find(item => item.value.toLowerCase() === valor)
+  if (selecionado) {
+    searchForm.setAttribute("data-search", selecionado.getAttribute("href"))
+  } else {
+    searchForm.setAttribute("data-search", "")
+  }
+})
+function redirecionar() {
+  console.log("redirecionar")
+  let href = searchForm.getAttribute("data-search")
+  if (href) {
+    console.log(href)
+    window.location.hash = href
+    searchForm.setAttribute("data-search", "")
+  } else{
+    alert("Nenhum item selecionado")
+  }
+}
 searchForm.addEventListener("input", (e) => {
   const valorDigitado = e.target.value.toLowerCase()
 
   itens.forEach((item) => {
-    // Verifica se o texto do item inclui o que foi digitado
     if (item.textContent.toLowerCase().includes(valorDigitado)) {
-      item.style.display = "block" // Mostra o item
+      item.style.display = "block" 
     } else {
-      item.style.display = "none" // Oculta o item
+      item.style.display = "none" 
     }
   })
 })
+// fim do codigo da caixa de pesquisa
